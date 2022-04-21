de6feecf205634568957597cb7485b392d16df9d
Kawin.M <Kawin.M@EXIND3010-14.allfon.in>
2022-04-21 10:41:46 +0530

    Object input,output

---
+++ Io_examples/src/com/training/services/BookService.java
@@ -0,0 +1,101 @@
+package com.training.services;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.training.model.Book;
+
+public class BookService {
+
+	public Object readFromStream(File file)
+	{
+		Object obj=null;
+		
+	try(ObjectInputStream objectInputStream=new ObjectInputStream(new FileInputStream(file)))
+		{
+			
+		obj=objectInputStream.readObject();
+			
+		}
+	
+	 catch(IOException | ClassNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		
+		return obj;
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	public boolean writeToStream(File file,Book book){
+		
+		boolean count=false;
+		try(ObjectOutputStream outputStream=new ObjectOutputStream(new FileOutputStream(file)))
+				{
+			
+		outputStream.writeObject(book);	
+			count=true;
+			
+		}
+		catch(IOException e)
+		{
+			
+			e.printStackTrace();
+		}
+		
+		return count;	
+		
+	}
+	
+	public boolean writeToFile(File file,Book book) 
+	{
+		boolean result=false;
+		
+		try (PrintWriter writer=new PrintWriter
+					(new FileWriter(file,true));)
+		{
+			
+			writer.println(book);
+			result=true;
+		}	
+		catch (IOException e) {
+
+			e.printStackTrace();
+		}
+		return result;
+	}	
+	
+	public List<Book>readFromFile(File file)
+	{
+		List<Book>bookList=new ArrayList<Book>();
+		
+		String line=null;
+		try(BufferedReader reader=new BufferedReader(new FileReader(file)))
+		{
+			
+			while( (line=reader.readLine())!=null  )
+			{
+				String []values=line.split(",");
+				
+		Book book=new Book(Integer.parseInt(values[0]),values[1],
+				values[2],Double.parseDouble(values[3]));
+				bookList.add(book);
+				
+		}
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+		return bookList;
+		
+	}
+	
+}
