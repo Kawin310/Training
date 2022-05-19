86ad7a457732794f7c889ae0f9b8c24acd904f66
Kawin.M <Kawin.M@EXIND3010-14.allfon.in>
2022-05-19 12:13:58 +0530

    Entity graph

---
+++ spring-jpa-entity-graph/src/main/java/com/example/demo/repos/AddressRepositiory.java
@@ -0,0 +1,20 @@
+package com.example.demo.repos;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.Address;
+
+public interface AddressRepositiory extends JpaRepository<Address, Integer> {
+
+	
+//@EntityGraph(attributePaths = {"user","city"},type = EntityGraph.EntityGraphType.LOAD)
+//List<Address>findAll();
+
+@EntityGraph(attributePaths = {"user"},type = EntityGraph.EntityGraphType.FETCH)
+List<Address>findAll();
+
+}
+//,"city"
