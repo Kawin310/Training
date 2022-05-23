755eaf86f04b3dbfc51e20b3d21686a00648e2f9
Kawin.M <Kawin.M@EXIND3010-14.allfon.in>
2022-05-23 09:54:00 +0530

    client-2

---
+++ spring-client-two/src/main/java/com/example/demo/controller/ClientController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.discovery.converters.Auto;
+
+@RestController 
+@RequestMapping(path = "/api/v1")
+public class ClientController {
+
+	private RestTemplate template;
+	
+	private String baseUrl="http://CLIENT-ONE/api/v1";
+	@Autowired
+	public ClientController(RestTemplate template) {
+		super();
+		this.template = template;
+	}
+	@GetMapping(path = "/client/books")
+	public String getBooks()
+	{
+		return this.template.getForObject(baseUrl+"/books",String.class);
+	}
+	
+	
+}
