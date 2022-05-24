469259acceb76b2a3a0149ad4a8c6561394a022b
Kawin.M <Kawin.M@EXIND3010-14.allfon.in>
2022-05-24 14:11:02 +0530

    Microservice Client 1

---
+++ microservice-one/src/main/java/com/example/demo/service/DriverService.java
@@ -0,0 +1,68 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Driver;
+import com.example.demo.ifaces.DriverRepository;
+
+@Service
+public class DriverService {
+
+//	@Autowired
+//	private Driver driver;
+	@Autowired
+	private DriverRepository repo;
+	
+	@Autowired
+	public DriverService(DriverRepository repo) {
+		super();
+		this.repo = repo;
+	}
+
+	public List<Driver> findAll() {
+		// TODO Auto-generated method stub
+		return repo.findAll();
+	}
+	
+	public Driver addDriver(Driver entity)
+	{
+		return this.repo.save(entity);
+	}
+	
+	public Driver findById(int id)
+	{
+		return this.repo.findById(id)
+				.orElseThrow(()->new RuntimeException(id +" not found"));
+	}
+
+	public void deleteById(int id)
+	{
+		
+		this.repo.deleteById(id);
+		//return null;			
+	}
+	
+	public List<Driver> findByDriverName(String srchName)
+	{
+		return this.repo.findByDriverName(srchName);
+	}
+	public List<Driver> srchByMobileNumber(long mobileNumber)
+	{
+		return this.repo.findByMobileNumber(mobileNumber);
+	}
+	
+	public List<Driver> srchByDriverRating(double rating)
+	{
+		return this.repo.searchByRating(rating);
+	}
+	
+	public int updateRating(int id,double updatedRating)
+	{
+		return this.repo.modifyRating(id,updatedRating);
+	}
+
+	
+}
