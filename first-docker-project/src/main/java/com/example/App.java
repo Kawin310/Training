32e95a533bc2095869643b99e462a219ea2dbdbc
Kawin.M <Kawin.M@EXIND3010-14.allfon.in>
2022-08-09 12:01:37 +0530

    Docker

---
+++ first-docker-project/src/main/java/com/example/App.java
@@ -0,0 +1,19 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+import java.util.*;
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+    List<String> names=Arrays.asList("kawin","Rishi","karthi","samy");
+    
+    names.forEach(System.out::println);
+    
+    
+    }
+}
